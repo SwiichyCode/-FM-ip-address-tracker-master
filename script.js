a270e65638a9851a345bdd1490b4d686d951255c
Swiichy <adlpromail@gmail.com>
2021-10-30 18:46:30 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+/*Your challenge is to build out this IP Address Tracker app and get it looking as close to the design as possible. 
+To get the IP Address locations, you'll be using the [IP Geolocation API by IPify](https://geo.ipify.org/). 
+To generate the map, we recommend using [LeafletJS](https://leafletjs.com/).*/
+
+
+console.log("Hello world!");
