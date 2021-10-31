ef9c2d27841f07052c43d9130ecf41620c064a35
Swiichy <adlpromail@gmail.com>
2021-10-31 11:06:16 +0100

    Add first logic

--- script.js
+++ script.js
@@ -1,6 +1,81 @@
 /*Your challenge is to build out this IP Address Tracker app and get it looking as close to the design as possible. 
 To get the IP Address locations, you'll be using the [IP Geolocation API by IPify](https://geo.ipify.org/). 
 To generate the map, we recommend using [LeafletJS](https://leafletjs.com/).*/
+//88.121.111.97
+const headerInformation = document.querySelector(".header-information");
 
+btnSearch.addEventListener("click", () => {
+  userDisplay();
+  fetchLocation();
+  headerInformation.style.display = "flex";
+});
 
-console.log("Hello world!");
+findIp = findIp.value;
+let userData = [];
+let latitude = 51.505;
+let longitude = -0.09;
+
+const fetchUser = async () => {
+  await fetch(
+    `https://geo.ipify.org/api/v2/country?apiKey=at_BfX6cUPn4TyiLYDXhWFYiF1xmLeg2&ipAddress=${findIp}`
+  )
+    .then((res) => res.json())
+    .then((data) => (userData = data));
+
+  console.log(userData);
+};
+
+const fetchLocation = async () => {
+  await fetch(`http://ip-api.com/json/${findIp}`)
+    .then((res) => res.json())
+    .then((data) => {
+      latitude = data.lat;
+      longitude = data.lon;
+    });
+};
+
+console.log(latitude);
+
+const userDisplay = async () => {
+  await fetchUser();
+
+  headerInformation.innerHTML = `
+    <div class="header-information__ip header-information-content">
+        <h3>ip address</h3>
+        <span>${userData.ip}</span>
+    </div>
+    <div class="header-information__location header-information-content">
+        <h3>location</h3>
+        <span>${userData.location.region}</span>
+    </div>
+    <div class="header-information__timezone header-information-content">
+        <h3>timezone</h3>
+        <span>${userData.location.timezone}</span>
+    </div>
+    <div class="header-information__isp header-information-content">
+        <h3>isp</h3>
+        <span>${userData.isp}</span>
+    </div>
+    `;
+};
+
+const myMap = async () => {
+  await fetchLocation();
+  var mymap = L.map("map").setView([latitude, longitude], 10);
+
+  L.tileLayer(
+    "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
+    {
+      attribution:
+        'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 18,
+      id: "mapbox/streets-v11",
+      tileSize: 512,
+      zoomOffset: -1,
+      accessToken:
+        "pk.eyJ1Ijoic3dpaWNoeWNvZGUiLCJhIjoiY2t2ZTQxMng2MTFvdjJxbzhzZnF1YnNpYSJ9.bFXSWqTiyB8I0TkrQFNL0w ",
+    }
+  ).addTo(mymap);
+};
+
+myMap();
